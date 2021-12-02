15307343e7dcdf0685e0426620feeb75d7b67e6d
lbflores <leticiabrasilflores@gmail.com>
2021-12-01 23:23:45 -0300

    createHtml

---
+++ createHtml.c
@@ -0,0 +1,91 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+typedef struct{
+    char palavra[50];
+    int frequencia;
+}Palavras;
+
+
+/* 
+criei essa função de gerar o HTML separado só para teste,
+assim que tiver funcionando o restante deve ser modificada
+
+*/
+
+void createHTML(Palavras palavras[], int numPalavras){ //no momento para funcionar deve receber um vetor de struct com palavra e frequencia
+
+
+//gravar arquivo texto
+    FILE *arq = fopen("index.html", "w"); //criar arquivo: w  r: leitura e a
+    if (arq == NULL)                      // 0, FALSE
+    {
+        printf("Erro ao abrir o aqruivo de saída \n");
+        exit(1);
+    }
+    //gerando html
+    fprintf(arq, "<html>\n");
+    fprintf(arq, "<title>Word Cloud");
+    fprintf(arq, "</title>\n");   
+    fprintf(arq, "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">\n");
+    fprintf(arq,"</head>\n");
+    fprintf(arq, "<body>\n");
+    fprintf(arq, "<div class=\"para\">\n");
+    
+    for (int i = 0; i< numPalavras; i++){
+        if (palavras[i].frequencia == 1){
+            fprintf(arq, "<span class=\"text1\">");
+            fprintf(arq, palavras[i].palavra);
+            fprintf(arq, "</span>\n");
+        }
+        else if (palavras[i].frequencia == 2 ){
+            fprintf(arq, "<span class=\"text2\">");
+            fprintf(arq, palavras[i].palavra);
+            fprintf(arq, "</span>\n");
+
+        }
+        else if (palavras[i].frequencia== 3){
+            fprintf(arq, "<span class=\"text3\">");
+            fprintf(arq, palavras[i].palavra);
+            fprintf(arq, "</span>\n");
+
+        }
+        else if (palavras[i].frequencia == 4){
+            fprintf(arq, "<span class=\"text4\">");
+            fprintf(arq, palavras[i].palavra);
+            fprintf(arq, "</span>\n");
+
+        }
+        else if (palavras[i].frequencia >= 5){
+            fprintf(arq, "<span class=\"text5\">");
+            fprintf(arq, palavras[i].palavra);
+            fprintf(arq, "</span>\n");
+
+        }
+
+    }
+
+    fprintf(arq, "</div>\n");
+    fprintf(arq, "</body>\n");
+    fprintf(arq, "</html>\n");
+
+    fclose(arq);
+
+}
+
+
+int
+main (int argc, char **argv)
+{
+
+Palavras palavras[] = {{"Java", 2}, {"C", 1}, {"C#", 3}, {"JavaScript", 4}, {"Python", 5}};
+
+
+
+createHTML(palavras, 5);
+
+
+}
